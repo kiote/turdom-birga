1fd8dd3bba27f269a68aa0d0af748e15e9850db7
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-11-24 10:46:16 +0300

    save page content

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var request = require('request');
+var fs = require('fs');
 
 app.get('/', function (req, res) {
-  res.send('Hello there!')
-})
+  // get content of birga's page
+  request('http://www.tourdom.ru/birga/go3/', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+      fs.writeFile('content/page.html', body, function(err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      res.send('saved!');
+    }
+  })
+});
 
-const port = 8080
+const port = 8080;
 app.listen(port, function () {
-  console.log('Example app listening on port' + port +'!')
+  console.log('Example app listening on port' + port +'!');
 });
