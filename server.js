6ec0061f5fb110ddb387bc1a13d7d5894f8d817b
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-11-25 10:52:42 +0300

    use osmosis to make raw html file

--- server.js
+++ server.js
@@ -2,19 +2,21 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var fs = require('fs');
+var osmosis = require('osmosis');
 
 app.get('/', function (req, res) {
   // get content of birga's page
-  request('http://www.tourdom.ru/birga/go3/', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-      fs.writeFile('content/page.html', body, function(err) {
-        if (err) {
-          console.log(err);
-        }
-      });
-      res.send('saved!');
-    }
-  })
+  osmosis.get('www.tourdom.ru/birga/go3/')
+         .find('tr > td.date')
+         .set('date')
+         .find('tr > td.direction:first')
+         .set('from')
+         .find('table.table-result > tr > td[3].direction')
+         .set('to')
+         .data(function(listing) {
+           console.log(listing);
+         });
+  res.send('done!');
 });
 
 const port = 8080;
